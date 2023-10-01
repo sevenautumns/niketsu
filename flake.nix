5db326c48034455e7e53652d7b8b5dee73e8e9bc
Sven Friedrich <sven@autumnal.de>
2023-10-01 22:02:13 +0200

    change to nixos-rebuild

--- flake.nix
+++ flake.nix
@@ -9,64 +9,53 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     niketsu-stable.url = "github:sevenautumns/niketsu";
     niketsu-nightly.url = "github:sevenautumns/niketsu";
 
     devshell.url = "github:numtide/devshell";
   };
 
-  outputs = { self, deploy-rs, nixpkgs, agenix, devshell, ... }@inputs:
+  outputs = { self, nixpkgs, agenix, devshell, ... }@inputs:
     let
       lib = nixpkgs.lib;
       pkgs = import nixpkgs {
         system = "x86_64-linux";
-        overlays = [ devshell.overlays.default deploy-rs.overlay ];
+        overlays = [ devshell.overlays.default ];
       };
-    in
-    {
+    in {
       nixosConfigurations.niketsu = lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          {
-            networking.hostName = "niketsu";
-            nixpkgs.overlays = [ deploy-rs.overlay ];
-          }
+          { networking.hostName = "niketsu"; }
           agenix.nixosModules.default
           ./server.nix
         ];
         specialArgs = { inherit inputs; };
       };
 
-      deploy.nodes.niketsu =
-        let
-          known-hosts = pkgs.writeText "known_hosts" ''
-            autumnal.de ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOB5kFkv5hNVA0nbeIo1LtGZDOORTH+lXrxq8h2EmI3e
-          '';
-        in
-        {
-          hostname = "autumnal.de";
-          fastConnection = false;
-          sshOpts = [ "-o" "UserKnownHostsFile=${known-hosts}" ];
-          profiles.system = {
-            sshUser = "admin";
-            path = deploy-rs.lib.x86_64-linux.activate.nixos
-              self.nixosConfigurations.niketsu;
-            user = "root";
-          };
-        };
-
       devShells.x86_64-linux.default = (pkgs.devshell.mkShell {
         name = "niketsu-deploy-shell";
-        packages = [ pkgs.deploy-rs.deploy-rs pkgs.openssh ];
+        packages = [ pkgs.openssh pkgs.nixos-rebuild ];
+        commands = [{
+          name = "deploy-niketsu";
+          category = "deploy";
+          command = let
+            known-hosts = pkgs.writeText "known_hosts" ''
+              autumnal.de ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOB5kFkv5hNVA0nbeIo1LtGZDOORTH+lXrxq8h2EmI3e
+            '';
+          in ''
+            NIX_SSHOPTS="-o UserKnownHostsFile=${known-hosts}"
+            export NIX_SSHOPTS
+            nixos-rebuild \
+              --flake .#niketsu \
+              --use-remote-sudo \
+              --target-host admin@autumnal.de \
+              --build-host admin@autumnal.de \
+              --use-substitutes \
+              switch 
+          '';
+          help = "Deploy new system config";
+        }];
       });
-
-      checks = builtins.mapAttrs
-        (system: deployLib: deployLib.deployChecks self.deploy)
-        deploy-rs.lib;
     };
 }
