df70b03d2639fe2fb781b6984ff9d3d7502209e6
Sven Friedrich <sven@autumnal.de>
2023-11-21 22:54:49 +0100

    add proxy

--- certificate.nix
+++ certificate.nix
@@ -4,6 +4,18 @@
   services.nginx = {
     enable = true;
     virtualHosts = {
+      "niketsu.de" = {
+        forceSSL = true;
+        enableACME = true;
+        locations = {
+          "/" = {
+            proxyPass = "https://sevenautumns.github.io/niketsu";
+            extraConfig = ''
+              proxy_set_header Host sevenautumns.github.io/niketsu;
+            '';
+          };
+        };
+      };
       "autumnal.de" = {
         forceSSL = true;
         enableACME = true;
